bceaf19e20202214bde9f35ec68c32a83befbd5c
GitHub Action <action@github.com>
2025-12-24 04:55:07 +0000

    Update devplan formula to version 0.53.0
    Updated formula with:
      - Version: 0.53.0

--- devplan.rb
+++ devplan.rb
@@ -1,30 +1,30 @@
 class Devplan < Formula
   desc "AI-powered development planning and task management CLI"
   homepage "https://github.com/devplaninc/devplan-cli"
-  version "0.52.0"
+  version "0.53.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/devplaninc/devplan-cli/releases/download/v#{version}/devplan-darwin-arm64-noautoupdate.tar.gz"
-      sha256 "20c3d07483a41936dd22de28b2ee660886c5ed7ca5766859a7fb9f7d8321f869"
+      sha256 "6fbae442aae0d1f683b6a0b15a8e85dbdeeb9edbe1f7f5cc8d73d3216f3a3c8c"
     end
 
     on_intel do
       url "https://github.com/devplaninc/devplan-cli/releases/download/v#{version}/devplan-darwin-amd64-noautoupdate.tar.gz"
-      sha256 "2d63b7107c2e0acb48425611df027d0fe42333ae11f7c67fb6107da069bfabd7"
+      sha256 "692974605ffac84a7ae369afc4b7309a72a04ca90f8c1f40c770bbc47d701e15"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/devplaninc/devplan-cli/releases/download/v#{version}/devplan-linux-arm64-noautoupdate.tar.gz"
-      sha256 "d5a24fd0a2a83272d5687843dc8a95c394f1f3a84c8325b8ff7746fd7f09000b"
+      sha256 "2a60beee5d287c00fd3c646cb36e498e0dfc605e92720afd1179277111ff58d7"
     end
 
     on_intel do
       url "https://github.com/devplaninc/devplan-cli/releases/download/v#{version}/devplan-linux-amd64-noautoupdate.tar.gz"
-      sha256 "7821810e8793d17bbb2fb9490813dc0805c0927f5e678c019c02a893e41ef1ad"
+      sha256 "805af2f780f444c7bf6022b9a4b8d278576300fc191e4537b5407ac67b0d80ab"
     end
   end
 
